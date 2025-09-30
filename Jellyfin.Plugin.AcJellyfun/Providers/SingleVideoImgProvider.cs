0832d7866de0723fff68320e0364e951b344df53
Sokwva <1519181530@qq.com>
2025-09-30 22:38:51 +0800

    add GetImages

--- Jellyfin.Plugin.AcJellyfun/Providers/SingleVideoImgProvider.cs
+++ Jellyfin.Plugin.AcJellyfun/Providers/SingleVideoImgProvider.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Jellyfin.Plugin.AcJellyfun.Model;
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
@@ -17,7 +18,7 @@ namespace Jellyfin.Plugin.AcJellyfun.Providers
         {
             public string Name => Plugin.PluginName;
             public bool Supports(BaseItem item) => item is Movie;
-            public IEnumerable<ImageType> GetSupportedImages(BaseItem baseItem) => new List<ImageType>
+            public IEnumerable<ImageType> GetSupportedImages(BaseItem item) => new List<ImageType>
             {
                 ImageType.Primary,
                 ImageType.Backdrop,
@@ -27,7 +28,50 @@ namespace Jellyfin.Plugin.AcJellyfun.Providers
 
             public async Task<IEnumerable<RemoteImageInfo>> GetImages(BaseItem item, CancellationToken cancellationToken)
             {
-                var itemSid = item.GetProviderId(BaseProviderId)
+                Log($"GetImages: {item?.Name} {item?.OriginalTitle} {item?.Path}");
+                if (item == null)
+                {
+                    return new List<RemoteImageInfo> { };
+                }
+                string acid = item.GetProviderId(BaseProviderId);
+                if (string.IsNullOrEmpty(acid) || !RegAcid.IsMatch(acid))
+                {
+                    return new List<RemoteImageInfo> { };
+                }
+
+                DougaInfoApiResp? resp = await FetchDougaInfo(acid, cancellationToken).ConfigureAwait(false);
+
+                if (resp == null)
+                {
+                    return new List<RemoteImageInfo> { };
+                }
+
+                if (resp.Code != 0)
+                {
+                    return new List<RemoteImageInfo> { };
+                }
+
+                Log($"Return GetImages of {acid}");
+                RemoteImageInfo resou = new()
+                {
+                    ProviderName = Name,
+                    Url = resp.Data.CoverURL,
+                    Language = "zh"
+                };
+
+                List<RemoteImageInfo> result = [];
+                resou.Type = ImageType.Primary;
+                result.Add(resou);
+
+                RemoteImageInfo resou2 = resou;
+                resou2.Type = ImageType.Art;
+                result.Add(resou2);
+
+                RemoteImageInfo resou3 = resou;
+                resou3.Type = ImageType.Backdrop;
+                result.Add(resou3);
+
+                return result;
             }
 
         }
